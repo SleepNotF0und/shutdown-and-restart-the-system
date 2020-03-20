4575d43e2098452c83a681bd94ad69f382ac4f0d
hazeem'yasseer <51271019+hazemyasr@users.noreply.github.com>
2020-03-21 00:24:20 +0200

    Add files via upload

---
+++ shutdown and restart.py
@@ -0,0 +1,8 @@
+﻿import os
+ch = int(input("Press 1 to Shutdown || Press 2 to Restart\n>>"))
+if ch == 1:
+    os.system("shutdown /s /t 1")
+elif ch ==2:
+    os.system("shutdown /r /t 1")
+else:
+    exit()
